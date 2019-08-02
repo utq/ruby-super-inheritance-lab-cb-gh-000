aae0ee4f373fe5cb1119780fac8c0852d0f271a1
Celal Cankaya <celalcankaya@outlook.com>
2019-08-02 16:56:04 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,12 @@
+class Student
+
+   def hello
+    puts "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+   def raise_hand
+    puts "Pick me!"
+  end
+
+ end
+ ProTip! Use n and p to navigate between commits in a pull request.
